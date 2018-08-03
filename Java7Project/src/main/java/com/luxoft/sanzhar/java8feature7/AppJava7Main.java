3a7dc6ee62b847ac69477cfb2d399239db2e87c8
sanzhar73ismailov <sanzhar73@gmail.com>
2018-08-03 06:19:47 +0200

    Restructure

---
+++ Java7Project/src/main/java/com/luxoft/sanzhar/java8feature7/AppJava7Main.java
@@ -0,0 +1,11 @@
+package com.luxoft.sanzhar.java8feature7;
+
+/**
+ *
+ * @author admin
+ */
+public class AppJava7Main {
+    public static void main(String[] args) {
+        
+    }
+}
--- Java8Project/src/main/java/com/luxoft/sanzhar/java8features/AppMain.java
+++ Java8Project/src/main/java/com/luxoft/sanzhar/java8features/AppMain.java
@@ -5,5 +5,7 @@ package com.luxoft.sanzhar.java8features;
  * @author admin
  */
 public class AppMain {
-
+    public static void main(String[] args) {
+        
+    }
 }
